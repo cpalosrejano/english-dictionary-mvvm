f024eaa189666c7d1ae4995325468f0f64aefa81
Cristian Palos Rejano <cpalosrejano@gmail.com>
2021-01-09 00:23:21 +0100

    Added toString methods in Word and Definition business object

--- app/src/main/java/com/cpalosrejano/englishdictionary/model/Word.kt
+++ app/src/main/java/com/cpalosrejano/englishdictionary/model/Word.kt
@@ -4,4 +4,10 @@ class Word (
     var word: String?,
     var definitions: List<Definition>?,
     var pronunciation: String?
-)
+) {
+
+    override fun toString(): String {
+        return "Definitions for $word:\n ${definitions.toString()}"
+    }
+
+}
