251a70ddb6cde37b34c9350cf2a0dfd0ca82f579
Cristian Palos Rejano <cpalosrejano@gmail.com>
2021-01-09 00:25:08 +0100

    Create function to retrieve word definition and livedata for observing result

--- app/src/main/java/com/cpalosrejano/englishdictionary/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/cpalosrejano/englishdictionary/ui/search/SearchViewModel.kt
@@ -1,11 +1,24 @@
 package com.cpalosrejano.englishdictionary.ui.search
 
 import androidx.lifecycle.*
+import com.cpalosrejano.englishdictionary.model.Word
 import com.cpalosrejano.englishdictionary.repository.word.WordRepository
+import kotlinx.coroutines.launch
 
 class SearchViewModel(
         private val repository: WordRepository
 ) : ViewModel() {
+
+    val definitions = MutableLiveData<Word>()
+
+    fun fetchDefinitionsFor(word: String) {
+        viewModelScope.launch {
+            val result = repository.getWord(word)
+            result?.let {
+                definitions.postValue(it)
+            }
+        }
+    }
 
     // FactoryClass to create new instance of this ViewModel
     class Factory(private val repository: WordRepository) : ViewModelProvider.NewInstanceFactory() {
